6f73cc12d080d9289b3944abbd55b13ce2157cd8
vsevalot <v.v.tetervak@gmail.com>
2024-09-07 13:27:19 +0200

    no trainling spaces?

--- init.sh
+++ init.sh
@@ -6,14 +6,14 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 sed -i 's/^ZSH_THEME=.*$/ZSH_THEME="avit"/' ~/.zshrc || echo 'ZSH_THEME="avit"' >> ~/.zshrc
 # Add plugins to .zshrc
 wget https://github.com/zsh-users/zsh-syntax-highlighting/archive/master.zip \
-    -O "zsh-syntax-highlighting.zip" \ 
-    && unzip "zsh-syntax-highlighting.zip" \ 
+    -O "zsh-syntax-highlighting.zip" \
+    && unzip "zsh-syntax-highlighting.zip" \
     && mv "zsh-syntax-highlighting-master" ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting \
     && rm "zsh-syntax-highlighting.zip"
 
 wget https://github.com/zsh-users/zsh-autosuggestions/archive/master.zip \
-    -O "zsh-autosuggestions.zip" \ 
-    && unzip "zsh-autosuggestions.zip" \ 
+    -O "zsh-autosuggestions.zip" \
+    && unzip "zsh-autosuggestions.zip" \
     && mv "zsh-autosuggestions-master" ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions \
     && rm "zsh-autosuggestions.zip"
 
