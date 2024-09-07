64b7cde23643b98dacf8f3ab4f26c6ad57f9eccc
vsevalot <v.v.tetervak@gmail.com>
2024-09-07 12:41:39 +0200

    init

---
+++ init.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install zsh -y
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Set ZSH_THEME in .zshrc
+sed -i 's/^ZSH_THEME=.*$/ZSH_THEME="avit"/' ~/.zshrc || echo 'ZSH_THEME="avit"' >> ~/.zshrc
+
+# Add plugins to .zshrc
+sed -i 's/^plugins=(.*)$/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/' ~/.zshrc || echo 'plugins=(git zsh-syntax-highlighting zsh-autosuggestions)' >> ~/.zshrc
+
+# Source .zshrc to apply the changes
+source ~/.zshrc
