fbddc3c32723182d441ee979780ebc67fd1b53c6
vsevalot <v.v.tetervak@gmail.com>
2024-09-07 12:46:05 +0200

    now new line?

--- init.sh
+++ init.sh
@@ -4,9 +4,7 @@ chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # Set ZSH_THEME in .zshrc
 sed -i 's/^ZSH_THEME=.*$/ZSH_THEME="avit"/' ~/.zshrc || echo 'ZSH_THEME="avit"' >> ~/.zshrc
-
 # Add plugins to .zshrc
 sed -i 's/^plugins=(.*)$/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/' ~/.zshrc || echo 'plugins=(git zsh-syntax-highlighting zsh-autosuggestions)' >> ~/.zshrc
-
 # Source .zshrc to apply the changes
 source ~/.zshrc
